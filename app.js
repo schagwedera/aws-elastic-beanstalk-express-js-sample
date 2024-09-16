3e46578f871ee136bcde9ad91916fbff86ade1bb
schagwedera <shepherd.chagwedera@gmail.com>
2024-09-16 20:14:24 +0200

    Update app.js
    adding Aussie

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to Canada!'));
+app.get('/', (req, res) => res.send('Welcome to Canada! But I want to welcome you to Australia as well'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
